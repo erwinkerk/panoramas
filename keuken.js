e634335403d3ceab5b2688aeffb318c7163d99e7
Erwin Kerk <erwin@blikgooien.nl>
2023-08-11 12:43:41 +0200

    added keuken

---
+++ keuken.js
@@ -0,0 +1,41 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "keuken_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 2,
+	        "restart": 5,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 65.29887789750923,
+	        "minpan": 0,
+	        "mintilt": -64.21684279463803
+	    },
+	"id": "keuken",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"keuken_tiles/keuken_t_1b_%0y_%0x.jpg"},"down":{"src":"keuken_tiles/keuken_t_1d_%0y_%0x.jpg"},"front":{"src":"keuken_tiles/keuken_t_1f_%0y_%0x.jpg"},"height":7936,"left":{"src":"keuken_tiles/keuken_t_1l_%0y_%0x.jpg"},"right":{"src":"keuken_tiles/keuken_t_1r_%0y_%0x.jpg"},"tilesize":496,"up":{"src":"keuken_tiles/keuken_t_1u_%0y_%0x.jpg"},"width":7936},{"back":{"src":"keuken_tiles/keuken_t_2b_%0y_%0x.jpg"},"down":{"src":"keuken_tiles/keuken_t_2d_%0y_%0x.jpg"},"front":{"src":"keuken_tiles/keuken_t_2f_%0y_%0x.jpg"},"height":3968,"left":{"src":"keuken_tiles/keuken_t_2l_%0y_%0x.jpg"},"right":{"src":"keuken_tiles/keuken_t_2r_%0y_%0x.jpg"},"tilesize":496,"up":{"src":"keuken_tiles/keuken_t_2u_%0y_%0x.jpg"},"width":3968},{"back":{"src":"keuken_tiles/keuken_t_3b_%0y_%0x.jpg"},"down":{"src":"keuken_tiles/keuken_t_3d_%0y_%0x.jpg"},"front":{"src":"keuken_tiles/keuken_t_3f_%0y_%0x.jpg"},"height":1984,"left":{"src":"keuken_tiles/keuken_t_3l_%0y_%0x.jpg"},"right":{"src":"keuken_tiles/keuken_t_3r_%0y_%0x.jpg"},"tilesize":496,"up":{"src":"keuken_tiles/keuken_t_3u_%0y_%0x.jpg"},"width":1984},{"back":{"src":"keuken_tiles/keuken_t_4b_%0y_%0x.jpg"},"down":{"src":"keuken_tiles/keuken_t_4d_%0y_%0x.jpg"},"front":{"src":"keuken_tiles/keuken_t_4f_%0y_%0x.jpg"},"height":992,"left":{"src":"keuken_tiles/keuken_t_4l_%0y_%0x.jpg"},"right":{"src":"keuken_tiles/keuken_t_4r_%0y_%0x.jpg"},"tilesize":496,"up":{"src":"keuken_tiles/keuken_t_4u_%0y_%0x.jpg"},"width":992},{"back":{"src":"keuken_tiles/keuken_t_5b_%0y_%0x.jpg"},"down":{"src":"keuken_tiles/keuken_t_5d_%0y_%0x.jpg"},"front":{"src":"keuken_tiles/keuken_t_5f_%0y_%0x.jpg"},"height":496,"left":{"src":"keuken_tiles/keuken_t_5l_%0y_%0x.jpg"},"right":{"src":"keuken_tiles/keuken_t_5r_%0y_%0x.jpg"},"tilesize":496,"up":{"src":"keuken_tiles/keuken_t_5u_%0y_%0x.jpg"},"width":496}],"multilevel":true,"preview":{"src":"keuken_tiles/keuken_preview.jpg"},"projection":"cubic"},
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
---
+++ keuken_controls.js
@@ -0,0 +1,92 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"actions": [
+        {
+            "func": "function(){ if (!this.viewer.fullEquirectangular()) return; this.viewer.panTo(this.viewer.getView().pan,0,100,1.0,0,'easeInOutSine',true); this.tween({'time': 1.0, 'lp': 0.0, 'transition': 'easeInOutSine', 'onInit' : function(a,dstParams){ a.lp=a.viewer.fisheyeDistortion(); }, 'onUpdate': function(a){ a.viewer.setFisheyeDistortion(a.lp);}}); }",
+            "id": "leaveLittlePlanet"
+        },
+        {
+            "func": "function(){ if (!this.viewer.fullEquirectangular()) return; this.viewer.panTo(this.viewer.getView().pan+90,90,150,1.0,0,'easeInOutSine',true); this.tween({'time': 1.0, 'lp': 1.0, 'transition': 'easeInOutSine', 'onInit' : function(a,dstParams){ a.lp=a.viewer.fisheyeDistortion(); }, 'onUpdate': function(a){ a.viewer.setFisheyeDistortion(a.lp);}}); }",
+            "id": "enterLittlePlanet"
+        },
+        {
+            "func": "function(a){ var id = this.viewer.currentMBId; if (!!id&&(a.id!=id)){this.viewer.action('hideMessage');} if (a.id!=id){ var s=this.viewer.get('globalData'); if (s&&s.messageBoxStyle){ a.style = s.messageBoxStyle; }this.viewer.currentMBId=null; if (!this.viewer.isVRModeEnabled()){ this.viewer.add('textbox',a); this.viewer.currentMBId=a.id; } } }",
+            "id": "showMessage"
+        },
+        {
+            "func": "function(){ if (this.viewer.currentMBId) this.viewer.remove(this.viewer.currentMBId); this.viewer.currentMBId=null; }",
+            "id": "hideMessage"
+        },
+        {
+            "func": "function(a){ var id = this.viewer.currentMBId; if (a.id!=id){ this.viewer.action('showMessage',a); } else { this.viewer.action('hideMessage'); } }",
+            "id": "toggleMessage"
+        }
+    ],
+"contextmenu": {
+        "id": "menu",
+        "items": [
+            {
+                "id": "fullscreenItem",
+                "onclick": "function(){ this.viewer.toggleFullscreen(); }",
+                "oninit": "function(){ this.caption = this.viewer.tr('Fullscreen'); if (!this.viewer.fullscreenSupported()) this.visible = false;  } "
+            },
+            {
+                "caption": "-"
+            },
+            {
+                "id": "normalView",
+                "onclick": "function(){  if (this.viewer.fisheyeDistortion()!=0.0){ this.viewer.action('leaveLittlePlanet'); } }",
+                "oninit": "function(){  this.caption = this.viewer.tr('Normal View'); }"
+            },
+            {
+                "id": "littlePlanetView",
+                "onclick": "function(){ if (this.viewer.fisheyeDistortion()!=1.0){ this.viewer.action('enterLittlePlanet'); } }",
+                "oninit": "function(){  this.caption = this.viewer.tr('LittlePlanet View'); }"
+            },
+            {
+                "caption": "-"
+            },
+            {
+                "id": "gyroItem",
+                "onclick": "function(){  var gb = this.get('gyrobutton'); if (gb){ gb.onclick(); } else { this.viewer.enableGyroscope(!this.viewer.gyroscopeEnabled()); } }",
+                "oninit": "function(){  this.caption = this.viewer.tr('Gyroscope Control'); }"
+            },
+            {
+                "caption": "-"
+            },
+            {
+                "onclick": "function(){ window.open('http://www.tshsoft.com','_blank'); }",
+                "oninit": "function(){  this.caption = this.viewer.tr('About PanoramaStudio...'); }"
+            }
+        ],
+        "onshow": "function(){ var view1 = this.getItem('normalView'); if (view1){ view1.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular()) ? true : false; } var view2 = this.getItem('littlePlanetView'); if (view2){ view2.visible = (this.viewer.webglAvailable && this.viewer.fullEquirectangular())?true:false; } var gyro = this.getItem('gyroItem'); if (gyro){ gyro.visible = this.viewer.gyroAvailable?true:false; }  this.update(); }",
+        "style": "background-color: rgba(255,255,255,0.8); box-shadow: 4px 4px 4px rgba(0,0,0,0.5); border-radius: 3px;"
+    },
+"events": {
+        "id": "mainEvents",
+        "onexit": "function(){  var d=this.viewer.get('localData'); if (!!d&&d.infoTextBox){ this.viewer.action('hideMessage'); } this.viewer.gyroWasEnabled = this.viewer.gyroAvailable&&this.viewer.gyroscopeEnabled();\t}",
+        "ongyroscopeavailable": "function(available){ this.viewer.gyroAvailable = available; var o = this.get('gyrobutton'); if(!!o){ o.setVisible(available&&this.viewer.panoType()==0); available&&o.onclick(); } } ",
+        "ongyroscopetoggle": "function(enabled){ this.viewer.action('updateGyroButton'); }",
+        "oninit": "function(){ var g = this.viewer.gallery(); this.viewer.hasGallery = ((!!g) && g.length>1); this.viewer.checkForGyroscope(); }",
+        "onplay": "function(){ var o = this.get('playbutton'); if (!!o){ o.sbackup = o.skin; o.shbackup = o.skinhover; o.sabackup = o.skinactive; o.skin = 'shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,0,0,28,28)'; o.skinhover = 'dim(-10px,-10px,48px,48px,48,48);comp(0.5);copy(defaultSkin,0,128,64,64,0,0,48,48);comp(1.0);shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,10,10,28,28)';\to.skinactive = 'dim(-10px,-10px,48px,48px,48,48);comp(0.9);copy(defaultSkin,0,128,64,64,0,0,48,48);comp(1.0);shadow(3,0,0,rgba(0,0,0,1));copy(defaultSkin,128,0,64,64,10,10,28,28)'; o.updateSkins(); } } ",
+        "onresize": "function(){ this.viewer.action('resizeMap');  }",
+        "onscenechanged": "function(){ var o = this.get('gyrobutton'); if(!!o){ o.setVisible(this.viewer.gyroAvailable&&this.viewer.panoType()==0); this.viewer.gyroAvailable&&this.viewer.panoType()==0&&this.viewer.gyroWasEnabled&&o.onclick(); } if (this.viewer.isVRModeEnabled()){this.viewer.enableGyroscope(!0);}}",
+        "onstartaudio": "function(senderId){ if (senderId=='gAudio'||senderId=='lAudio'){var o = this.get('audiobutton'); if (!!o){ o.skin = o.playskin; o.skinhover = o.playskinhover; o.skinactive = o.playskinactive; o.updateSkins(); }} } ",
+        "onstop": "function(){ var o = this.get('playbutton'); if (!!o){ o.skin = o.sbackup; o.skinhover = o.shbackup; o.skinactive = o.sabackup; o.updateSkins(); } } ",
+        "onstopaudio": "function(senderId){ if (senderId=='gAudio'||senderId=='lAudio'){var o = this.get('audiobutton'); if (!!o){ o.skin = o.pauseskin; o.skinhover = o.pauseskinhover; o.skinactive = o.pauseskinactive; o.updateSkins(); }} } ",
+        "onuseswebgl": "function(available){ this.viewer.webglAvailable = available; if (available){ var vr = this.get('vrButton'); vr&&vr.setVisible(true); } } "
+    },
+"settings": {
+        "safeareamargin": "-8 -8 -8 -8"
+    },
+"translate": {
+        "de": {
+            "About PanoramaStudio...": "&Uuml;ber PanoramaStudio...",
+            "Fullscreen": "Vollbild",
+            "Gyroscope Control": "Gyroskop-Steuerung",
+            "LittlePlanet View": "LittlePlanet-Ansicht",
+            "Normal View": "Normale Ansicht"
+        }
+    }
+}
+}
