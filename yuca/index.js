1ffa330b43ed9e827c329c1014281f6878ee62e2
Erwin Kerk <erwin@blikgooien.nl>
2024-05-12 20:37:37 +0200

    added camping yuca v2

--- yuca/index.js
+++ yuca/index.js
@@ -26,7 +26,7 @@ PanoramaStudioViewerParams = {
 	        "maxpan": 360,
 	        "maxtilt": 90,
 	        "minpan": 0,
-	        "mintilt": -33.31809813451769
+	        "mintilt": -34.30505260446183
 	    },
 	"id": "index",
 	"image":{"baseindex":1,"bitmap":[{"back":{"src":"index_tiles/index_t_1b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_1d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_1f_%0y_%0x.jpg"},"height":5854,"left":{"src":"index_tiles/index_t_1l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_1u_%0y_%0x.jpg"},"width":5854},{"back":{"src":"index_tiles/index_t_2b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_2d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_2f_%0y_%0x.jpg"},"height":2927,"left":{"src":"index_tiles/index_t_2l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_2u_%0y_%0x.jpg"},"width":2927},{"back":{"src":"index_tiles/index_t_3b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_3d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_3f_%0y_%0x.jpg"},"height":1464,"left":{"src":"index_tiles/index_t_3l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_3u_%0y_%0x.jpg"},"width":1464},{"back":{"src":"index_tiles/index_t_4b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_4d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_4f_%0y_%0x.jpg"},"height":732,"left":{"src":"index_tiles/index_t_4l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_4u_%0y_%0x.jpg"},"width":732},{"back":{"src":"index_tiles/index_t_5b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_5d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_5f_%0y_%0x.jpg"},"height":366,"left":{"src":"index_tiles/index_t_5l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_5u_%0y_%0x.jpg"},"width":366}],"multilevel":true,"preview":{"src":"index_tiles/index_preview.jpg"},"projection":"cubic"},
@@ -40,8 +40,8 @@ PanoramaStudioViewerParams = {
 	    ],
 	"view": {
 	        "hfov": 90,
-	        "pan": 180,
-	        "tilt": 0
+	        "pan": 144.81,
+	        "tilt": 47.4
 	    }
 },
 "version": "4.1"
