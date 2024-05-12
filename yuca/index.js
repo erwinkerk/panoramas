92377679e4dc94a696cc0f937b64836500acdf9e
Erwin Kerk <erwin@blikgooien.nl>
2024-05-12 20:26:10 +0200

    added camping yuca

---
+++ yuca/index.js
@@ -0,0 +1,49 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "index_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 2,
+	        "restart": 5,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 90,
+	        "minpan": 0,
+	        "mintilt": -33.31809813451769
+	    },
+	"id": "index",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"index_tiles/index_t_1b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_1d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_1f_%0y_%0x.jpg"},"height":5854,"left":{"src":"index_tiles/index_t_1l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_1u_%0y_%0x.jpg"},"width":5854},{"back":{"src":"index_tiles/index_t_2b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_2d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_2f_%0y_%0x.jpg"},"height":2927,"left":{"src":"index_tiles/index_t_2l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_2u_%0y_%0x.jpg"},"width":2927},{"back":{"src":"index_tiles/index_t_3b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_3d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_3f_%0y_%0x.jpg"},"height":1464,"left":{"src":"index_tiles/index_t_3l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_3u_%0y_%0x.jpg"},"width":1464},{"back":{"src":"index_tiles/index_t_4b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_4d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_4f_%0y_%0x.jpg"},"height":732,"left":{"src":"index_tiles/index_t_4l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_4u_%0y_%0x.jpg"},"width":732},{"back":{"src":"index_tiles/index_t_5b_%0y_%0x.jpg"},"down":{"src":"index_tiles/index_t_5d_%0y_%0x.jpg"},"front":{"src":"index_tiles/index_t_5f_%0y_%0x.jpg"},"height":366,"left":{"src":"index_tiles/index_t_5l_%0y_%0x.jpg"},"right":{"src":"index_tiles/index_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"index_tiles/index_t_5u_%0y_%0x.jpg"},"width":366}],"multilevel":true,"preview":{"src":"index_tiles/index_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Camping Yuca",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
